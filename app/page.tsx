a0128387f8dd014dd1e17284de19bd95c6341765
thegrtnx <thegrtnx@duck.com>
2024-02-20 21:34:12 +0100

    👌updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,25 @@ export default function Home() {
 			<p>
 				I love drinking H<sub>2</sub>0 and I <i>breathe</i> in O<sup>2</sup>
 			</p>
+			<p>
+				Print this page by pressing <kbd>ctrl + p</kbd> on your keyboard
+			</p>
+			<p>
+				Gretness loves coding in{" "}
+				<pre>
+					<code>html, css, javascript, react, next.js</code>
+				</pre>
+			</p>
+			<p>
+				I really really so much hate Tinubu of <abbr title="All Progressive Congress">APC</abbr>
+			</p>
+			<p>
+				I live at <address>No.10, Jidetilowo street, off Munachimso busttop, United Kingom, Akure.</address>
+			</p>
+
+			<p>
+				<bdo dir="ltr">This is a left to riht and can be a right to left</bdo>
+			</p>
 		</>
 	);
 }
